545d602fe52f77fe8b4d2d77a200fe9677f0a842
Ugur Armagan <ugurarmagan93@gmail.com>
2021-03-08 07:53:18 -0600

    adds tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          light: '#C7C9FF',
+          DEFAULT: '#7479DC',
+          dark: '#3D3E66',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
